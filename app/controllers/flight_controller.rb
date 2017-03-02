93ad5734741c48898f8e2d20c0ac0801d6c0640d
caitlin ashtari and haley swain <caitlin+haley@>
2017-03-01 16:42:26 -0800

    stuff

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -2,6 +2,10 @@ class FlightController < ApplicationController
 
   def index
     flight_object = GetFlight.new("US", "USD", "english", "SFO", "NYC", "anytime", "anytime")
-    @flight = flight_object.get_flight()
+    @flight = flight_object.get_flight()["Dates"]["OutboundDates"]
+    @location = flight_object.get_flight()["Places"]
+    
+
+
   end
 end
--- app/services/get_flight.rb
+++ app/services/get_flight.rb
@@ -12,6 +12,6 @@ class GetFlight
 
   def get_flight
     response=HTTParty.get("http://partners.api.skyscanner.net/apiservices/browsedates/v1.0/#{@country}/#{@currency}/#{@locale}/#{@originPlace}/#{@destinationPlace}/#{@outboundPartialDate}/#{@inboundPartialDate}??apikey=#{ENV['apikey']}")
-    response["Dates"]["OutboundDates"]
+    response
     end
   end
